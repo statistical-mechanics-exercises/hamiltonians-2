d843b6cb05b205c8e9c3385531e3e04795eec901
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-15 11:17:24 +0000

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,14 @@
+import unittest
+from main import *
+
+class UnitTests(unittest.TestCase) :
+    def test_eng(self) :
+    for i in range(2**5) :
+        num, spins = i, 5*[0]
+        for j in range(5) :
+            spins[j] = np.floor( num / 2**(4-j) )
+            num = num - spins[j]*2**(4-j)
+            if spins[j]==0 : spins[j] = -1
+        energy = -spins[0]*spins[len(spins) - 1 ]
+        for i in range(0,len(spins)-1) : energy = energy - spins[i]*spins[i+1]
+        self.assertTrue( hamiltonian( spins )==energy, "Hamiltonian gives the wrong value for the energy" )
