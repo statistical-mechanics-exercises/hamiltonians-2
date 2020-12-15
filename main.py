55b716d8432c3b486e5bc329d9d8013c2cc318d5
Gareth Tribello <gareth.tribello@gmail.com>
2020-12-15 13:11:39 +0000

    Added answer

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def hamiltonian( spins ) :
   energy = 0
   # Your code goes here
-  
+  energy = -spins[0]*spins[-1]
+  for i in range(1,len(spins)) : energy = energy - spins[i-1]*spins[i] 
   return energy 
   
 allup, alldown = 10*[1], 10*[-1]
